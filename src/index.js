3fdd404f314a6871e45bccd283de4de8d33de6e5
TB831 <druu831@gmail.com>
2019-03-27 15:21:56 -0700

    imported and setup redux to our react app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStore(rootReducer, {})}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
