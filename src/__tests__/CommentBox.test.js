3d9b49bd8ff6783607ba726e6bc28c7138563249
TB831 <druu831@gmail.com>
2019-03-27 14:33:51 -0700

    wrote test to simulate value added to the textarea and check if it gets passed to our props

--- src/__tests__/CommentBox.test.js
+++ src/__tests__/CommentBox.test.js
@@ -16,3 +16,11 @@ it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 })
+
+it('has a text area that users can type in', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment'}
+  });
+  wrapped.update()  // Rerenders our component
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment'); // finds textarea, checks the prop value, make sure it equals test comment
+})
--- src/containers/CommentBox.js
+++ src/containers/CommentBox.js
@@ -7,7 +7,7 @@ class CommentBox extends Component {
       comment: ''
     }
   }
-  
+
   submitHandler = (e) => {
     e.preventDefault(); // Prevents page from reloading
     
@@ -22,7 +22,7 @@ class CommentBox extends Component {
         <h5 className="grey-text text-darken-3">Create New Comment</h5>
         <div className="input-field">
           <label className="white-text" htmlFor="title">Write something interesting...</label>
-          <textarea onChange={(e) => this.setState({ comment: e.target.value })} className="materialize-textarea" />
+          <textarea onChange={(e) => this.setState({ comment: e.target.value })} value={this.state.comment} className="materialize-textarea" />
         </div>
         <div>
           <button className="btn pink z-depth-2">Create</button>
