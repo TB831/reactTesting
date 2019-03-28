68c01b033f7aad38cece11534f2fc696745e317c
TB831 <druu831@gmail.com>
2019-03-28 09:53:25 -0700

    configured reduxPromise. added Middleware

--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
+import reduxPromise from 'redux-promise';
 
 
 export default ({ children, initialState={} }) => {
+  const store = createStore(rootReducer, initialState, applyMiddleware(reduxPromise));
+
   return (
-    <Provider store={createStore(rootReducer, initialState)}>
+    <Provider store={store}>
       {children}
     </Provider>
   )
