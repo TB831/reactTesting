a75656727b1b959c8dc0b282b879be63916ad3e9
TB831 <druu831@gmail.com>
2019-03-28 10:22:22 -0700

    rewired CommentBox to fetch comments from JSONPlaceholder

--- src/containers/CommentBox.js
+++ src/containers/CommentBox.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { saveComment } from '../actions/index';
+import * as actions from '../actions/index';
 
 class CommentBox extends Component {
   constructor() {
@@ -10,35 +10,32 @@ class CommentBox extends Component {
     }
   }
 
-  submitHandler = (e) => {
-    e.preventDefault(); // Prevents page from reloading
-    
+  onCreateClick = () => {
     // Call our action creator
     // And save the comment
     this.props.saveComment(this.state.comment);
     this.setState({ comment: '' });
   }
 
+  onFetchClick = () => {
+    this.props.fetchComments();
+  }
+
   render() { 
     return (
-      <form onSubmit={this.submitHandler} className="card-panel teal lighten-2">
+      <form onSubmit={(e) => e.preventDefault()} className="card-panel teal lighten-2">
         <h5 className="grey-text text-darken-3">Create New Comment</h5>
         <div className="input-field">
           <label className="white-text" htmlFor="title">Write something interesting...</label>
           <textarea onChange={(e) => this.setState({ comment: e.target.value })} value={this.state.comment} className="materialize-textarea" />
         </div>
         <div>
-          <button className="btn pink z-depth-2">Create</button>
+          <button onClick={this.onCreateClick} className="btn pink z-depth-2">Create</button>
+          <button onClick={this.onFetchClick} className="btn red z-depth-2 right">Fetch Comments</button>
         </div>
       </form>
     );
   }
 }
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    saveComment: (comment) => dispatch(saveComment(comment))
-  }
-}
  
-export default connect(null, mapDispatchToProps)(CommentBox);
+export default connect(null, actions)(CommentBox);
